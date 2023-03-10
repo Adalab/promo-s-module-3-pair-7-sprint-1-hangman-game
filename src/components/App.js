0ad7332ac7d4741f1519b37a8acd61e0ac9130d2
Nuria Castaño <codesalnuria@gmail.com>
2023-03-10 11:29:49 +0100

    add handle user input

--- src/components/App.js
+++ src/components/App.js
@@ -8,26 +8,34 @@ function App() {
   const [word, setWord] = useState('katakroker');
   const [userLetters, setUserLetters] = useState([]);
 
-  const renderSolutionsLetters = () =>{
-    const wordLetters = word.split('');
-    return wordLetters.map((eachWordLetters) =><li class="letter"></li>);
-  }
-
   const handleClick = (ev) => {
     ev.preventDefault();
     setNumberOfErrors(numberOfErrors+1);
   }
 
   const handleChangeLastLetter = (ev) => {
-    ev.preventDefault();
+    // ev.preventDefault();
     const inputLastLetter = ev.target.value;
-    if (/[a-zA-Z\Ñ\ñ]/.test(inputLastLetter)){
-      setLastLetter(ev.target.value);
+    const regExp=/^[A-Za-zñÑáéíóúÁÉÍÓÚüÜ\s]*$/;
+    const pattern = regExp.test(inputLastLetter);
+  
+    if (pattern){
+      setLastLetter(inputLastLetter);
     } else {
-      setLastLetter('');
+      const letters=[...lastLetter];
+      letters.push(inputLastLetter)
+      setUserLetters(letters)
     }
-    userLetters.push(setLastLetter);
   }
+
+  const renderSolutionsLetters = () =>{
+    const wordLetters = word.split('');
+    return wordLetters.map((eachWordLetter, index) =>{
+    if(userLetters.includes(eachWordLetter)){ 
+      return <li key={index} className="letter">{eachWordLetter}</li>}
+    else {return <li key={index} className="letter"></li>}
+  })}
+
 
   return <div className="App">{
     <div className="page">
@@ -54,7 +62,7 @@ function App() {
           </div>
           <form className="form">
             <label className="title" htmlFor="last-letter">Escribe una letra:</label>
-            <input onChange={handleChangeLastLetter}
+            <input onChange={handleChangeLastLetter()}
               autocomplete="off"
               className="form__input"
               maxlength="1"
